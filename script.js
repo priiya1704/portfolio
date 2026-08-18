95a58519a2dfc2463a94fdd4e5a3616779c81211
priya <priiya1777@gmail.com>
2026-08-18 11:39:47 +0530

    Make portfolio responsive

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
         }
     });
 });
